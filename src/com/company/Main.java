441a7ea05bb4e3288fb72301afabcf28539329ef
dleon <dleon@students.mchenry.edu>
2019-01-28 00:29:39 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// using println
+        System.out.println("Hello, my name is Dan.");
+        System.out.println("I love to program. I have wanted to be a programmer since I was 10 years old, thats 31 " +
+                "years");
+        System.out.println("My brothers and I have always dreamed about making our own video games.");
+        System.out.println("This is me doing my part in achieving that dream!");
+    }
+}
