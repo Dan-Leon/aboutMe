6511a1e498254a14ab03beed99e1f1188cbfd044
dleon <dleon@students.mchenry.edu>
2019-01-28 00:39:27 -0600

    adding an extra comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// using println
+    // this is me making changes
         System.out.println("Hello, my name is Dan.");
-        System.out.println("I love to program. I have wanted to be a programmer since I was 10 years old, thats 31 " +
+        System.out.println("I love to program. I have wanted to be a programmer since I was 10 years old, that's 31 " +
                 "years");
         System.out.println("My brothers and I have always dreamed about making our own video games.");
         System.out.println("This is me doing my part in achieving that dream!");
